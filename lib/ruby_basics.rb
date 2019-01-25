54ccf29280a2cd8bc64c19184163e99ac6bc8a98
Olivia Auzenne <oauzenne@gmail.com>
2019-01-25 21:04:11 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,13 +1,16 @@
 def division(num1, num2)
- 
+  return (num1.to_i/num2.to_i)
 end
+division(42,7)
 
-def assign_variable(value)
-  
+
+def assign_variable(name)
+  name = name
 end
+assign_variable("Bob")
 
-def argue
-  
+def argue(phrase="I'm right and you are wrong!")
+  return phrase
 end
 
 def greeting
